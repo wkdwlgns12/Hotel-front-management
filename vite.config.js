0111385c657a822af53796736aac5134965607c3
wkdwlgns12 <jjg530@kbu.ac.kr>
2025-12-22 10:12:08 +0900

    int

--- vite.config.js
+++ vite.config.js
@@ -1,24 +1,10 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-
-  server: {
-    host: true,
-    port: 5173,
-    watch: {
-      usePolling: true, // Docker에서 파일 변경 감지
-    },
-  },
   css: {
-    devSourcemap: true,
-    preprocessorOptions: {
-      scss: {
-        sourceMap: true,
-        sourceMapContents: true,
-      },
-    },
-  },
-});
+    devSourcemap: true
+  }
+})
